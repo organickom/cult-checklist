3ba122c05a9163f93e176500c07ef81a099eb02f
organickom <jeffreyblair65@gmail.com>
2025-09-15 16:34:28 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Adjust base if deploying to GitHub Pages project site: '/<REPO_NAME>/'
 export default defineConfig({
   plugins: [react()],
-  base: '', // set to '/cult-checklist/' (your repo name) if using project pages
-})
+  base: '/cult-checklist/',
+  })
